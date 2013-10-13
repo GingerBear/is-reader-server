c45bc9f1126004d8b9175f970c952a5c3861e302
unknown <bbbear444@gmail.com>
2013-10-13 17:59:28 -0400

    push to heroku

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-//mongoose.connect('mongodb://bbbear444:123qweasd@ds049898.mongolab.com:49898/is-reader');
-mongoose.connect('mongodb://localhost/is_reader');
+mongoose.connect('mongodb://bbbear444:123qweasd@ds049898.mongolab.com:49898/is-reader');
+//mongoose.connect('mongodb://localhost/is_reader');
 exports.mongoose = mongoose;
 exports.schema = mongoose.Schema;
