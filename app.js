97fd1145d367ed2a8bbde9554f2afd0e9fe8b19d
unknown <bbbear444@gmail.com>
2013-10-21 02:42:46 -0400

    fix cookie auth bug

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
-  app.use(express.cookieParser('neil secret'));
+  app.use(express.cookieParser('neil_secret'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -71,47 +71,55 @@ app.get('/logout', function(req, res){
 });
 
 app.get('/reader', function(req, res){
-  res.render('reader');
+  if (req.cookies.rememberme) {
+    res.render('reader');
+  }else{
+    res.render('login'); 
+  }
 });
 
 app.post('/upload', function(req, res){
-  var i = 1
-    , len = Object.keys(req.files).length;
-  for (var key in req.files) {
-    // save pdf 
-    if (req.files[key].type === 'application/pdf') {
-      (function(file, i, len){
-        fs.readFile(file.path, function (err, data) {
-          if (!err) {            
-            var fileName = Date.now() + ".pdf";
-            // for windows
-            //var newPath = __dirname + "\\public\\data\\pdf\\" + fileName;
-            // for linux
-            var newPath = __dirname + "/public/data/pdf/" + fileName;
-            fs.writeFile(newPath, data, function (err) {
-              if (!err) {
-                console.log(file.name.split('.')[0] +" :: "+ fileName);
-                book.addBook(file.name.split('.')[0], fileName, function(id) {
-                  book.addUserBook(req.cookies.user_id, id, function() {
-                    if (i === len) {
-                      return res.json({ success: true });
-                    }
-                    console.log(i + 'th file saved');                
+  if (req.cookies.rememberme) {
+    var i = 1
+      , len = Object.keys(req.files).length;
+    for (var key in req.files) {
+      // save pdf 
+      if (req.files[key].type === 'application/pdf') {
+        (function(file, i, len){
+          fs.readFile(file.path, function (err, data) {
+            if (!err) {            
+              var fileName = Date.now() + ".pdf";
+              // for windows
+              //var newPath = __dirname + "\\public\\data\\pdf\\" + fileName;
+              // for linux
+              var newPath = __dirname + "/public/data/pdf/" + fileName;
+              fs.writeFile(newPath, data, function (err) {
+                if (!err) {
+                  console.log(file.name.split('.')[0] +" :: "+ fileName);
+                  book.addBook(file.name.split('.')[0], fileName, function(id) {
+                    book.addUserBook(req.cookies.user_id, id, function() {
+                      if (i === len) {
+                        return res.json({ success: true });
+                      }
+                      console.log(i + 'th file saved');                
+                    });
                   });
-                });
-              } else {
-                console.log(i + 'th file save error: ' + err);
-              }
-            });
-          } else  {
-            console.log('read file error: ' + err);
-            return res.json({ success: false });
-          }
-        });
-      })(req.files[key], i, len)
-    }    
-    i += 1;
-  };
+                } else {
+                  console.log(i + 'th file save error: ' + err);
+                }
+              });
+            } else  {
+              console.log('read file error: ' + err);
+              return res.json({ success: false });
+            }
+          });
+        })(req.files[key], i, len)
+      }    
+      i += 1;
+    };
+  }else{
+    res.render('login'); 
+  }
 });
 
 
